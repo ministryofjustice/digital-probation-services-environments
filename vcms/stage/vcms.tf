ae1cb0659a46a9dbd479cea75b9102c316fe3c68
Robin Sillem <robin.sillem@digital.justiice.gov.uk>
2018-05-08 09:05:18 +0100

    Adds security groups for ELB and EC2

--- vcms/stage/vcms.tf
+++ vcms/stage/vcms.tf
@@ -8,11 +8,76 @@ variable "eb_environment_name" {
   default = "vcms-stage"
 }
 
+# Security groups
+
+resource "aws_security_group" "elb" {
+  name        = "${var.application_name}-elb"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "ELB"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(module.tags.tags, map("Name", "${var.application_name}_elb"))}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "${var.application_name}-ec2"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "elasticbeanstalk EC2 instances"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.elb.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${merge(module.tags.tags, map("Name", "${var.application_name}_ec2"))}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Elastic beanstalk environments
+
 # This resource is shared with multiple environments
 resource "aws_elastic_beanstalk_application" "eb_app" {
   name        = "${var.application_name}"
   description = "Test of beanstalk deployment"
 }
+
+# Stage environment
 
 resource "aws_elastic_beanstalk_environment" "eb_environment" {
   name = "${var.eb_environment_name}"
@@ -50,6 +115,21 @@ resource "aws_elastic_beanstalk_environment" "eb_environment" {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "ServiceRole"
     value     = "aws-elasticbeanstalk-service-role"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SecurityGroups"
+    value     = "${aws_security_group.ec2.id}"
+  }
+  setting {
+    namespace = "aws:elb:loadbalancer"
+    name      = "ManagedSecurityGroup"
+    value     = "${aws_security_group.elb.id}"
+  }
+  setting {
+    namespace = "aws:elb:loadbalancer"
+    name      = "SecurityGroups"
+    value     = "${aws_security_group.elb.id}"
   }
   tags        = "${module.tags.tags}"
 }
